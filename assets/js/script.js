27bb3853137cb91f3d6ca0ec422532d9c2e125d4
Guillaume Buisson <guillaume.buisson.simplon@gmail.com>
2020-07-27 17:25:59 +0200

    popular movies/end movies list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,107 @@
-let key = mykey;
+import {mykey} from '../../key.js';
+
 let myinput = document.getElementById('my-input');
 let movielist = document.getElementById('my-movie-list');
+let bodymovie = document.getElementById('body-movies');
+let mypopularlist = document.getElementById('my-popular-list');
 
 myinput.value = '';
 
 myinput.addEventListener('keyup', (e) => 
 {
-        findmovie(`https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&page=1&include_adult=false&query=${myinput.value}`);
-        
+    refreshlist();
+    if(myinput.value.length > 0)
+    {
+        findmovie(`https://api.themoviedb.org/3/search/movie?api_key=${mykey}&language=en-US&page=1&include_adult=false&query=${myinput.value}`);
+    }
 })
 
 const findmovie = (url) =>
 {
     fetch(url)
-        .then(function(response)
+        .then((response) =>
         {
             return response.json();
         })
-        .then(function(transformation){
-            console.log(transformation);
+        .then((transformation) =>{
             refreshlist();
             addlistmovie(transformation);
         })
 }
 
+const popularmovies = (url) =>
+{
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((transformation) => {
+            console.log(transformation);
+            addpopularmovie(transformation);
+        })
+}
+
+popularmovies(`https://api.themoviedb.org/3/movie/popular?api_key=${mykey}&language=en-US&page=1`);
+
+const addpopularmovie = (myjson) => {
+
+    for (let i = 0; i < myjson.results.length; i++) {
+        let myli = document.createElement('li');
+        myli.innerHTML = `<img alt="" src="https://image.tmdb.org/t/p/w500/${myjson.results[i].poster_path}">`
+        mypopularlist.append(myli);
+    }
+}
+
 const addlistmovie = (myjson) =>
 {
-    for (let i = 0; i < myjson.results.length; i++) {
-        let mytitle = document.createElement('div');
-        mytitle.innerHTML = `<p>${myjson.results[i].title}</p>`;
-        movielist.append(mytitle);
-    }
+    refreshlist();
 
-    if(myjson.results.length <= 0)
+    if(myjson.results.length >= 4)
+    {
+        let myjsonsliced = myjson.results.slice(0, 3);
+        for (let i = 0; i < myjsonsliced.length; i++) {
+            let mytitle = document.createElement('div');
+            mytitle.innerHTML = `<a><img alt="" src="https://image.tmdb.org/t/p/w500/${myjsonsliced[i].poster_path}"></img> <p>${myjsonsliced[i].title}</p></a>`;
+            movielist.append(mytitle);
+        }
+
+        for (let i = 0; i < myjson.results.length; i++) {
+            let mymovies = document.createElement('div');
+            mymovies.innerHTML = `<img alt="" src="https://image.tmdb.org/t/p/w500/${myjson.results[i].poster_path}"></img> <p>${myjson.results[i].overview}</p> <p>${myjson.results[i].title}</p>`;
+            bodymovie.append(mymovies);
+        }
+    }
+    else if(myjson.results.length <= 0)
     {
         let mytitle = document.createElement('div');
         mytitle.innerHTML = `<p>Aucun resultats trouvé...</p>`;
         movielist.append(mytitle);
+        bodymovie.append(mytitle);
     }
 }
 
+
 const refreshlist = () => 
 {
-    for (let i = 0; i < movielist.children.length; i++) 
+    for (let i = movielist.children.length -1; i >= 0 ; i--) 
     {
         movielist.removeChild(movielist.children[i]);
     }
+    for (let i = bodymovie.children.length -1; i >= 0 ; i--)
+    {
+        bodymovie.removeChild(bodymovie.children[i]);
+    }
 }
+
+document.addEventListener('click', e => {
+
+    if(e.target.id === 'my-input')
+    {
+        movielist.style.display = 'inline-block';
+    }
+    else 
+    {
+        movielist.style.display = 'none';
+    }
+
+})
--- key.js
+++ key.js
@@ -1,1 +1,3 @@
 const mykey = 'e05c3a1077bd3e7c5bf305329f6c2d9a';
+
+export {mykey};
