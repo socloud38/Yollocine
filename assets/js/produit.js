ec00e0f286d4046d22f686e5fcbd0ad4eed45f9e
Guillaume Buisson <guillaume.buisson.simplon@gmail.com>
2020-08-04 14:43:27 +0200

    comment

--- assets/js/produit.js
+++ assets/js/produit.js
@@ -1,174 +1,287 @@
+//import de la clé API via mon modul key.js
 import {mykey} from '../../key.js';
 
 //metavar
 let metatitle = document.querySelectorAll('.meta-title');
 let metadescr = document.querySelectorAll('.meta-descr');
 
+//
 let mybodytxt = document.getElementById('body-txt');
 let mytxtdiv = document.getElementById('my-txt-div');
 
+//
 let myimgdiv = document.getElementById('my-img-div');
 let mycardimg = document.getElementById('card-img');
 
+//var de mon titre d'onglet
 let mynavtitle = document.getElementById('my-nav-title');
 
+//recupération de l'ID du film choisis dans mon localstorage
 let myid = localStorage.getItem('id');
+//recupération du status de mes input radio (films/series) choisis dans mon localstorage
 let mystyle = localStorage.getItem('movie');
 
+//var mon image
 let myimg = document.getElementById('my-img');
+//var mon titre
 let mytitle = document.getElementById('my-title');
+//var du reseumé
 let myresume = document.getElementById('synopsis');
+//var du status du film
 let mystatus = document.getElementById('status');
+//var de ma date
 let mydate = document.getElementById('date');
+//var de la durée du film
 let myruntime = document.getElementById('runtime');
+//var du genre du film
 let mygenre = document.getElementById('genre');
-
+//var d'une alternative si il n'y a pas de resumé disponible
 let altresume = 'Il n\'y a pas d\'informations pour ce films...';
 
+//fetch par rapport a l'ID du film trouvé dans le localstorage
 const findid = (url) =>
 {
+	//fetch
 	fetch(url)
+		//then
 		.then((response) =>
 		{
+			//transformation en JSON
 			return response.json();
 		})
+		//then
 		.then((transformation) =>{
 			console.log(transformation);
+			//determine si films ou série 
 			if(mystyle === 'true')
 			{
+				//films
 				datamanagemovies(transformation);
 			}
 			else 
 			{
+				//séries
 				datamanageseries(transformation);
 			}
 		});
 };
 
-console.log(mystyle);
-
+//test si films ou série
 if(mystyle === 'true')
 {
+	//films
 	findid(`https://api.themoviedb.org/3/movie/${myid}?api_key=${mykey}&language=fr`);
 }
 else if(mystyle === 'false')
 {
+	//séries
 	findid(`https://api.themoviedb.org/3/tv/${myid}?api_key=${mykey}&language=fr`);
 }
 
+//fct pour formater la date en "Day Month(en lettre) Year"
 const myformat = (mydate) => {
+	//création d'un tableau avec tout les mois "null" sert a faire tomber janvier sur 1 et non 0
 	let mymonths = [null,'Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'];
+	//slice de la date pour récuperer juste le jour de sortie
 	let myday = mydate.slice(-2);
+	//slice de la date pour récuperer just le mois
 	let mymonth = mydate.slice(5,7);
+	//le mois = l'index correspondant dans mon tableau parsint pour transformer les 01 en 1 etc...
 	mymonth = mymonths[parseInt(mymonth)];
+	//recuperation de l'année
 	let myyear = mydate.slice(0,4);
+	//concatenne la date
 	let mynewdate = `${myday} ${mymonth} ${myyear}`;
+	//return ma date formaté
 	return mynewdate;
 };
 
+//fct pour formater le runtime en hh 'H' mm 'M'
 const myformath = (mytime) => {
+	//recupérer les heures en divisant le runetime par 60
 	let myhours = Math.floor(mytime / 60);
+	//recupération des minute en utilisant un modulo
 	let myminutes = mytime % 60;
+	//concatenne le nouveau runtime
 	let mynewtime = `${myhours}h${myminutes}min`;
+	//return ma runtime formaté
 	return mynewtime;
 };
 
+//fct qui manage toute les donées du films obtenue
 const datamanagemovies = (myjson) => {
+	//met l'image2 du film en fond
 	mybodytxt.style.backgroundImage = `URL(https://image.tmdb.org/t/p/original/${myjson.backdrop_path})`;
+	//met le titre du film en titre de l'onglet
 	mynavtitle.innerText = myjson.original_title;
+	//SEO schema
+	mynavtitle.itemtype = 'https://schema.org/name';
+	//met l'image1 du film dans la carte prévu
 	myimg.src = `https://image.tmdb.org/t/p/original/${myjson.poster_path}`;
+	//SEO schema
+	myimg.itemtype = 'https://schema.org/image';
+	//met une alternative si il n'y a pas d'images
 	myimg.alt = 'Pas d\'image pour ce film';
+	//met le titre dans le H1 prevu
 	mytitle.innerText = myjson.original_title;
+	//SEO schema
+	mytitle.itemtype = 'https://schema.org/name';
+	//met le titre en capitale
 	mytitle.style.textTransform = 'uppercase';
-	console.log(myjson.genres.lenght);
+	//test si il y a un genre pour le films obtenue
 	if(myjson.genres[0] != null)
 	{
+		//met le genre trouver en position 0 du tableau du JSON
 		mygenre.innerText = `${myjson.genres[0].name}`;
+		//test si il y en a un 2eme
 		if(myjson.genres[1] != null)
 		{
+			//l'affiche
 			mygenre.innerText = `${myjson.genres[0].name} ${myjson.genres[1].name}`;
 		}
+		//SEO schema
+		mygenre.itemtype = 'https://schema.org/genre';
+		//font size du genre a 20px
 		mygenre.style.fontSize = '20px';
 	}
+	//test si le statut est en production pour le mettre en rouge sinon vert
 	if(myjson.status === 'In Production'){ mystatus.style.color = 'red';}
+	//SEO schema
+	mystatus.itemtype = 'https://schema.org/status';
+	//met le status du film dans l'HTML
 	mystatus.innerText = myjson.status;
+	//met la date dans le HTML
 	mydate.innerHTML = `Sortie le : <b>${myformat(myjson.release_date)}</b>`;
+	//SEO schema
+	mydate.itemtype = 'https://schema.org/Date';
+	//met la date en italic
 	mydate.style.fontStyle = 'italic';
+	//test si il y a un resumé ou non
 	if(myjson.overview === '')
 	{
+		//si il y en a pas on met l'alternative crée
 		myresume.innerText = altresume;
+		//SEO schema
+		myresume.texttype = 'https://schema.org/description';
 	}
 	else 
 	{
+		//sinon affiché celui disponible
 		myresume.innerText = myjson.overview;
+		//SEO schema
+		myresume.texttype = 'https://schema.org/description';
 	}
+	//font size du resumé
 	myresume.style.fontSize = '16px';
+
+	//SEO schema
+	myruntime.itemtype = 'https://schema.org/Duration';
+	//met la durée du film formaté dans le HTML
 	myruntime.innerHTML = `Durée : <b>${myformath(myjson.runtime)}</b>`;
 
 	//meta
-	console.log(myjson.overview.slice(0, 100));
 	for (let i = 0; i < metatitle.length; i++) {
 
+		//SEO meta titre et resumé coupé a 100caracteres
 		metatitle[i].content = myjson.original_title;
 		metadescr[i].content = myjson.overview.slice(0, 100);
 
 	}
 };
 
+//fct séries
 const datamanageseries = (myjson) => {
+	//met l'image2 de la série en fond
 	mybodytxt.style.backgroundImage = `URL(https://image.tmdb.org/t/p/original/${myjson.backdrop_path})`;
+	//met le titre dans l'onglet
 	mynavtitle.innerText = myjson.name;
+	//met l'image1 de la série dans la carte prévu
 	myimg.src = `https://image.tmdb.org/t/p/original/${myjson.poster_path}`;
+	//met une alternative a l'image
 	myimg.alt = 'Pas d\'image pour cette série';
+	//met le titre de la série
 	mytitle.innerText = myjson.name;
+	//en capitale
 	mytitle.style.textTransform = 'uppercase';
+	//test si il y a un genre ou non
 	if(myjson.genres.lenght > 0)
 	{
+		//ajout du 1er genre de la list
 		mygenre.innerText = `${myjson.genres[0].name}`;
+		//font size 20px
 		mygenre.style.fontSize = '20px';
 	}
+	//test si le status est encore en cour de production alors rouge sinon vert
 	if(myjson.status === 'Returning Series'){ mystatus.style.color = 'red';}
+	//met le status dans l'HTML
 	mystatus.innerText = myjson.status;
+	//met la date formatée dans l'HTML
 	mydate.innerHTML = `Sortie le : <b>${myformat(myjson.first_air_date)}</b>`;
+	//en italic
 	mydate.style.fontStyle = 'italic';
+	//test si il y a ou non un résumé
 	if(myjson.overview === '')
 	{
+		//non alors je met mon alternative crée
 		myresume.innerText = altresume;
 	}
 	else 
 	{
+		//oui je met le resumé disponible
 		myresume.innerText = myjson.overview;
 	}
+	//font size 16px
 	myresume.style.fontSize = '16px';
+	//affiche le nombres de saisons et d'épisodes
 	myruntime.innerHTML = `Nombre de saisons : <b>${myjson.number_of_seasons}</b> </br> Nombre d'épisodes : <b>${myjson.number_of_episodes}</b>`;
 };
 
+//event au click sur l'image (la carte)
 myimg.addEventListener('click', () => {
+	//test si l'image est déja scale ou non
 	let imgbool = false;
 
+	//cache le TXT
 	mytxtdiv.style.display = 'none';
+	//reduit la widht de la grid a 50%
 	myimgdiv.style.width = '50%';
+	//met 100 de viewHeight
 	myimgdiv.style.height = '100vh';
+	//un backgroundcolor noir opacity 0.7
 	myimgdiv.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+	//enleve les grid bootstrap
 	myimgdiv.classList.remove('col-12');
+	//pareil
 	myimgdiv.classList.remove('col-md-6');
+	//fais un scaling de 1.3 sur l'image
 	mycardimg.style.transform = ('scale(1.3)');
 
+	//laisse 0,1s avant de pouvoir désactiver le zoom de l'image
 	setTimeout( () => {
+		//l'image peut etre dezoomer
 		imgbool = true;
 	}, 100)
 
+	//event click sur tout le document
 	document.addEventListener('click', e => {
+		//si l'image peut etre dezoomé
 		if(imgbool === true)
 		{
 			console.log(e.target.id);
+			//si l'user click partout sauf sur l'image celle ci ce dezoom
 			if(e.target.id != 'my-img');
 			{
+				//affiche le TXT
 				mytxtdiv.style.display = '';
+				//enleve le background noir opacity 0.7
 				myimgdiv.style.backgroundColor = '';
+				//grid bootstrap
 				myimgdiv.classList.add('col-12');
+				//pareil
 				myimgdiv.classList.add('col-md-6');
+				//remet le scaling a 1 de l'img
 				mycardimg.style.transform = ('scale(1)');
+				//l'img peut a nouveau etre zoomé
 				imgbool = false;
 			}
 		}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,39 @@
+//clé API obtenue via le module key.js
 import {mykey} from '../../key.js';
 
+//mes variables getID
+
+//input
 let myinput = document.getElementById('my-input');
+//div movielist (list déroulante en dessous de l'input)
 let movielist = document.getElementById('my-movie-list');
+//div des films a l'affichage dans la page
 let bodymovie = document.getElementById('body-movies');
+//div carroussel
 let mypopularbody = document.getElementById('my-popular-body');
+//UL carroussel
 let mypopularlist = document.getElementById('my-popular-list');
+//Nav de la pagination
 let myindexpage = document.getElementById('my-page-index');
+//input radio films
 let radiomovies = document.getElementById('radio-movies');
+//input radio movie
 let radioserie = document.getElementById('radio-series');
+//text au dessus du carroussel
 let mytxtcarou = document.getElementById('carou-txt');
+//text pagination
 let pagenumbertxt = document.getElementById('page-number');
-
+//pagenumber = la page actuel entre 1 et 999
 let pagenumber = 1;
-
+//boolean si film(true) ou series(false)
 let movies = true;
 
+//event au click sur le document (içi pour récuperer l'ID au click et le stocker)
 document.addEventListener('click', e => {
 	localStorage.setItem('id', e.target.id);
 	console.log(e.target.id);
 
+	//si film ou serie
 	if(radioserie.checked)
 	{
 		movies = false;
@@ -31,22 +46,32 @@ document.addEventListener('click', e => {
 	}
 });
 
+//s'assurer que l'input est vide au chargement de la page
 myinput.value = '';
+//check l'input radio film au chargement
 radiomovies.checked = true;
 
+//mes URL des films/series populaire pour les caroussels
 let myurlpopularmovie = `https://api.themoviedb.org/3/movie/popular?api_key=${mykey}&language=fr&page=1`;
 let myurlpopularseries = `https://api.themoviedb.org/3/tv/popular?api_key=${mykey}&language=fr&page=1`;
 
+//event quand j'appuye sur n'importe quel touche dans l'input
 myinput.addEventListener('keyup', () => 
 {
+	//fct refresh
 	refreshlist();
+	//initialisation de la page 1
 	pagenumber = 1;
+	//écrire le numéro de page dans le txt
 	pagenumbertxt.innerText = pagenumber;
+	//si il y a quelque chose dans l'input
 	if(myinput.value.length > 0)
 	{
+		//URL pour fetch film/serie
 		let myurlmovies = `https://api.themoviedb.org/3/search/movie?api_key=${mykey}&language=fr&page=${pagenumber}&include_adult=false&query=${myinput.value}`;
 		let myurlseries = `https://api.themoviedb.org/3/search/tv?api_key=${mykey}&language=fr&page=${pagenumber}&query=${myinput.value}&include_adult=false`;
 
+		//check si films ou séries et fetch
 		if(radiomovies.checked)
 		{
 			findmovie(myurlmovies);
@@ -57,6 +82,8 @@ myinput.addEventListener('keyup', () =>
 		}
 	}
 
+	//fais disparaitre le carroussel si l'input n'est pas vide
+	//et les bouton pagination si il est vide
 	if(myinput.value != '')
 	{
 		mypopularbody.style.display = 'none';
@@ -69,16 +96,23 @@ myinput.addEventListener('keyup', () =>
 	}
 });
 
+//fct fetch pour les films
 const findmovie = (url) =>
 {
+	//fetch
 	fetch(url)
+		//then response
 		.then((response) =>
 		{
+			//transforme en JSON
 			return response.json();
 		})
+		//then du JSON
 		.then((transformation) =>{
 			console.log(transformation);
+			//refresh ma list deroulante
 			refreshlist();
+			//check si film ou séries
 			if(radiomovies.checked)
 			{
 				addlistmovie(transformation);
@@ -90,220 +124,343 @@ const findmovie = (url) =>
 		});
 };
 
+//fetch des films populaires
 const popularmovies = (url) =>
 {
+	//fetch
 	fetch(url)
+		//then
 		.then((response) => {
+			//transforme en JSON
 			return response.json();
 		})
+		//then
 		.then((transformation) => {
 			console.log(transformation);
+			//add les bonne img dans le carrou
 			addpopularmovie(transformation);
 		});
 };
 
+//event au click de l'input radio films
 radiomovies.addEventListener('click', () => {
+	//actualise le txt au dessus du carrou
 	mytxtcarou.innerText = 'Films Populaires :';
+	//supprime les carrou actuel
 	for (let i = mypopularlist.children.length -1; i >= 0 ; i--) {
 		mypopularlist.removeChild(mypopularlist.children[i]);
 	}
+	//ajoute les nouveau carrou (mise a jour des images)
 	popularmovies(myurlpopularmovie);
 });
 
+//event au click du radio serie
 radioserie.addEventListener('click', () => {
+	//mise a jour du txt au dessus du carrou
 	mytxtcarou.innerText = 'Séries Populaires :';
+	//supprime le carrou actuel (films)
 	for (let i = mypopularlist.children.length -1; i >= 0 ; i--) {
 		mypopularlist.removeChild(mypopularlist.children[i]);
 	}
+	//mise a jour des imgs du carrou
 	popularmovies(myurlpopularseries);
 });
 
+//fct carrou met les images des films populaire dans le carrou
 popularmovies(myurlpopularmovie);
 
+//fct qui crée les li des films populaire et les ajoute dans le ul du carrou
 const addpopularmovie = (myjson) => {
 
+	//boucle for qui créer les li pour chaque film populaire fetch en JSON
 	for (let i = 0; i < myjson.results.length; i++) {
+		//création des li
 		let myli = document.createElement('li');
+		//ajout d'un a avec sont Href qui mene a la page produit
 		myli.innerHTML = `<a href="produit.html"><img id='${myjson.results[i].id}' alt="" src="https://image.tmdb.org/t/p/original/${myjson.results[i].poster_path}"></a>`;
+		//append du li dans la list ul du carrou
 		mypopularlist.append(myli);
 	}
 };
 
+//fct qui ajoute les films dans la movielist et dans la page
 const addlistmovie = (myjson) =>
 {
+	//refresh de la list et de la page
 	refreshlist();
 
+	//partie de la liste en dessous de l'input
 	if(myjson.results.length >= 4)
 	{
+		//découpe du nombre de resultats au 3premier pour la liste
 		let myjsonsliced = myjson.results.slice(0, 3);
+		//boucle for dans le JSON coupé
 		for (let i = 0; i < myjsonsliced.length; i++) {
+			//check si une image et disponible sinon pas d'affichage
 			if(myjson.results[i].poster_path != null)
 			{
+				//création d'une div pour chaque element de la liste
 				let mytitle = document.createElement('div');
+				//on lui met sa balise A et son HREF
 				mytitle.innerHTML = `<a id='${myjsonsliced[i].id}' href="produit.html"><img id='${myjsonsliced[i].id}' alt="" src="https://image.tmdb.org/t/p/original/${myjsonsliced[i].poster_path}"></img> <p id='${myjsonsliced[i].id}'>${myjsonsliced[i].title}</p></a>`;
+				//append dans la list
 				movielist.append(mytitle);
 			}
 		}
 
+		//boucle for pour la list de film de la page
 		for (let i = 0; i < myjson.results.length; i++) {
+			//check si il y a une image sinon ne pas afficher
 			if(myjson.results[i].poster_path != null)
 			{
+				//recupération de la date de sortie et découpe pour avoir juste l'année
 				let daterel = myjson.results[i].release_date.slice(0,4);
+				//création de la div principale
 				let mymovies = document.createElement('div');
-				mymovies.itemtype=("http://schema.org/Movie");
+				//SEO schema
+				mymovies.itemtype= "http://schema.org/Movie";
+				//création d'un A
 				let mymoviesa = document.createElement('a');
+				//ajout de la class
 				mymovies.classList.add('div-movie-link');
+				//class
 				mymoviesa.classList.add('my-links-movies');
+				//class bootstrap responsive grid
 				mymovies.classList.add('col-12');
+				//class bootstrap responsive grid
 				mymovies.classList.add('col-sm-4');
+				//class bootstrap responsive grid
 				mymovies.classList.add('col-md-3');
+				//class bootstrap responsive grid
 				mymovies.classList.add('col-lg-2');
+				//ajout d'un Href dans le A créé precedement
 				mymoviesa.href = 'produit.html';
+				//je stock l'ID du film cliqué dans l'ID de la balise A
 				mymoviesa.id = myjson.results[i].id;
+				//ajout de l'image du film en background de ma div
 				mymovies.style.backgroundImage = `URL('https://image.tmdb.org/t/p/original/${myjson.results[i].poster_path}')`;
+				//edition du HTML de ma div (titre + date entre parantheses)
 				mymovies.innerHTML = `<div><h1>${myjson.results[i].title} (${daterel})</h1></div>`;
+				//append du A dans ma div
 				mymovies.append(mymoviesa);
+				//append de la div dans la div prévu en HTML
 				bodymovie.append(mymovies);
 			}
 		}
 	}
+	//si il n'y a pas de resultat de la recherche
 	else if(myjson.results.length <= 0)
 	{
+		//création d'une div
 		let mytitle = document.createElement('div');
+		//color text de la div = blanc
 		mytitle.style.color = 'white';
+		//margin top de la div 50px
 		mytitle.style.marginTop = '50px';
+		//edition de l'html de la div
 		mytitle.innerHTML = '<p>Aucun resultats trouvés...</p>';
+		//append de ma div dans la list et dans la page
 		movielist.append(mytitle);
 		bodymovie.append(mytitle);
 	}
 };
 
+//fct pour les séries
 const addlistserie = (myjson) =>
 {
+	//refresh de la list
 	refreshlist();
 
+	//test si il y a plus de 4 resultats
 	if(myjson.results.length >= 4)
 	{
+		//decoupe des resultats
 		let myjsonsliced = myjson.results.slice(0, 3);
+		//boucle for de la list pour les serie (en dessous de l'input)
 		for (let i = 0; i < myjsonsliced.length; i++) {
-			if(myjson.results[i].poster_path != null)for (let i = movielist.children.length -1; i >= 0 ; i--) 
+			//test si img sinon pas affiché
+			if(myjson.results[i].poster_path != null)
 			{
+			//boucle for pour remove la list d'avant
+			for (let i = movielist.children.length -1; i >= 0 ; i--) 
+			{
+				//remove les enfants de la list
 				movielist.removeChild(movielist.children[i]);
 			}
-			{
+				//création d'une div principale
 				let mytitle = document.createElement('div');
+				//edition de son HTML
 				mytitle.innerHTML = `<a id='${myjsonsliced[i].id}' href="produit.html"><img id='${myjsonsliced[i].id}' alt="" src="https://image.tmdb.org/t/p/original/${myjsonsliced[i].poster_path}"></img> <p id='${myjsonsliced[i].id}'>${myjsonsliced[i].name}</p></a>`;
+				//append de la div dans la list
 				movielist.append(mytitle);
 			}
 		}
 
+		//bucle for pour la page principale des séries
 		for (let i = 0; i < myjson.results.length; i++) {
+			//test si img sinon pas afficher
 			if(myjson.results[i].poster_path != null)
 			{
+				//recuperation de l'année
 				let daterel = myjson.results[i].first_air_date.slice(0,4);
+				//création d'une div
 				let myseries = document.createElement('div');
+				//création d'un A
 				let myseriesa = document.createElement('a');
+				//SEO schea serie
+				myseries.itemtype=("https://schema.org/TVSeries");
+				//class
 				myseries.classList.add('div-movie-link');
+				//class
 				myseriesa.classList.add('my-links-movies');
+				//class bootstrap responsive
 				myseries.classList.add('col-12');
+				//class bootstrap responsive
 				myseries.classList.add('col-sm-4');
+				//class bootstrap responsive
 				myseries.classList.add('col-md-3');
+				//class bootstrap responsive
 				myseries.classList.add('col-lg-2');
+				//Href pour le A vers page produit
 				myseriesa.href = 'produit.html';
+				//mettre l'ID du film dans l'ID du A
 				myseriesa.id = myjson.results[i].id;
+				//image du film en background
 				myseries.style.backgroundImage = `URL('https://image.tmdb.org/t/p/original/${myjson.results[i].poster_path}')`;
+				//edition du HTML de ma div
 				myseries.innerHTML = `<div><h1>${myjson.results[i].name} (${daterel})</h1></div>`;
+				//append du A dans la div
 				myseries.append(myseriesa);
+				//append de la div
 				bodymovie.append(myseries);  
 			}
 		}
 	}
+	//si aucun resultats
 	else if(myjson.results.length <= 0)
 	{
+		//création d'une div
 		let mytitle = document.createElement('div');
+		//text color = blanc
 		mytitle.style.color = 'white';
+		//margin top 50px
 		mytitle.style.marginTop = '50px';
+		//edition du HTML
 		mytitle.innerHTML = '<p>Aucun resultats trouvés...</p>';
+		//append dans la liste
 		movielist.append(mytitle);
+		//append dans la page
 		bodymovie.append(mytitle);
 	}
 };
 
+//fct refresh list
 const refreshlist = () => 
 {
+	//boucle for qui parcours ma list (en dessous de l'input)
 	for (let i = movielist.children.length -1; i >= 0 ; i--) 
 	{
+		//supprime tout les enfant de ma list
 		movielist.removeChild(movielist.children[i]);
 	}
+	//boucle for qui parcours ma page de recherche 
 	for (let i = bodymovie.children.length -1; i >= 0 ; i--)
 	{
+		//supprime tout les enfant (films precedement recherché)
 		bodymovie.removeChild(bodymovie.children[i]);
 	}
 };
 
+//event click sur le document pour afficher ou non la list
 document.addEventListener('click', e => {
 
+	//test si l'user click sur l'input ou non
 	if(e.target.id === 'my-input')
 	{
+		//l'user click sur l'input list = affiché
 		movielist.style.display = 'inline-block';
 	}
+	//sinon
 	else 
 	{
+		//l'user click n'importe ou mais pas sur l'input list = caché
 		movielist.style.display = 'none';
 	}
 
 });
 
+//variable de mes boutons pagination right
 let mybtnpge1 = document.getElementById('btn-page-Up');
+//left
 let mybtnpge2 = document.getElementById('btn-page-Down');
 
+//event au click sur mon bouton right pagination
 mybtnpge1.addEventListener('click', () => {
 
+	//incremente la variable page
 	pagenumber ++;
+	//test si la variable atteint 1000 alors = 999
 	if(pagenumber >= 1000)
 	{
 		pagenumber = 999;
 	}
-        
+		
+	//test si film
 	if(radiomovies.checked)
 	{
+		//url du fetch pour mes films
 		let myurlmovies = `https://api.themoviedb.org/3/search/movie?api_key=${mykey}&language=fr&page=${pagenumber}&include_adult=false&query=${myinput.value}`;
             
-		console.log('film');
+		//la paginationTXT prend le numéro de la variable
 		pagenumbertxt.innerText = pagenumber;
+		//lance le fetch des films
 		findmovie(myurlmovies);
 	}
+	//séries
 	else
 	{
+		//URL de mes séries
 		let myurlseries = `https://api.themoviedb.org/3/search/tv?api_key=${mykey}&language=fr&page=${pagenumber}&query=${myinput.value}&include_adult=false`;
-            
-		console.log('serie');
+			
+		//la paginationTXT prend le numéro de la variable
 		pagenumbertxt.innerText = pagenumber;
+		//lance le fetch des séries
 		findmovie(myurlseries);
 	}
 });
 
+//boutton left
 mybtnpge2.addEventListener('click', () => {
 
+	//décrementation de la variable
 	pagenumber--;
+	//test si la variable arrive a 0 alors = 1 (il n'y a pas de page 0)
 	if(pagenumber === 0)
 	{
 		pagenumber = 1;
 	}
+	//test si film
 	if(radiomovies.checked)
 	{
+		//url du fetch de mes films
 		let myurlmovies = `https://api.themoviedb.org/3/search/movie?api_key=${mykey}&language=fr&page=${pagenumber}&include_adult=false&query=${myinput.value}`;
-        
-		console.log('film');
+		
+		//la paginationTXT prend le numéro de la variable
 		pagenumbertxt.innerText = pagenumber;
+		
+		//lance le fetch des films
 		findmovie(myurlmovies);
 	}
+	//serie
 	else
 	{
+		//url pour le fetch de mes séries
 		let myurlseries = `https://api.themoviedb.org/3/search/tv?api_key=${mykey}&language=fr&page=${pagenumber}&query=${myinput.value}&include_adult=false`;
-        
-		console.log('serie');
+		
+		//la paginationTXT prend le numéro de la variable
 		pagenumbertxt.innerText = pagenumber;
+		
+		//lance le fetch des séries
 		findmovie(myurlseries);
 	}
 });
